4c446bb5f0b702c548e9be3cb755a72082c902e5
mtnyrgn <metinyorgun1996@gmail.com>
2020-09-18 12:01:50 +0300

    add: product model added.

---
+++ API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public long Count { get; set; }
+
+    }
+}
